54a67c19e38f37cd92bfd31bb059c726897ad41c
dhlocse <dhloc.se@gmail.com>
2022-07-09 23:46:16 +0700

    lession1-helloworld

---
+++ helloworld.go
@@ -0,0 +1,16 @@
+package main
+import ("fmt")
+
+func main() {
+	var num int = 4
+	var str string = "loc"
+	a := 5
+	b := "long"
+
+  fmt.Println("Hello World!")
+  
+  fmt.Println(num)
+  fmt.Println(str)
+  fmt.Println(num * a)
+  fmt.Println(str + " " + b)
+}
